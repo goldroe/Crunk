304db749a00fc6e68975014dfb28a10df9c3d5fa
Carlos Rivera <cr4185320@gmail.com>
2024-12-08 20:27:56 -0600

    tree generation

--- src/crunk/world_gen.cpp
+++ src/crunk/world_gen.cpp
@@ -2,12 +2,27 @@
 internal void init_world_generator(World_Generator *generator, int seed) {
     //@Note Height Map
     generator->height_noise = new noise::module::Perlin();
-    // generator->height_noise->SetSeed(seed);
+    generator->height_noise->SetSeed(seed);
     generator->height_noise_map = new noise::utils::NoiseMap();
 
     generator->height_noise_map_builder = new noise::utils::NoiseMapBuilderPlane();
     generator->height_noise_map_builder->SetSourceModule(*generator->height_noise);
     generator->height_noise_map_builder->SetDestNoiseMap(*generator->height_noise_map);
+
+    //@Note Tree Map
+    generator->tree_noise = new noise::module::Perlin();
+    generator->tree_noise->SetSeed(seed);
+    generator->tree_noise->SetOctaveCount(20);
+    generator->tree_noise->SetLacunarity(2.0);
+    generator->tree_noise->SetPersistence(1.0);
+    generator->tree_noise->SetSeed(seed);
+    generator->tree_noise->SetFrequency(0.5);
+    generator->tree_noise_map = new noise::utils::NoiseMap();
+
+    generator->tree_noise_map_builder = new noise::utils::NoiseMapBuilderPlane();
+    generator->tree_noise_map_builder->SetSourceModule(*generator->tree_noise);
+    generator->tree_noise_map_builder->SetDestNoiseMap(*generator->tree_noise_map);
+
 }
 
 internal void update_world_generator(World_Generator *generator, s32 region_x, s32 region_z) {
@@ -17,6 +32,10 @@ internal void update_world_generator(World_Generator *generator, s32 region_x, s
     generator->height_noise_map_builder->SetDestSize(256, 256);
     generator->height_noise_map_builder->SetBounds(1.0, 8.0, 2.0, 10.0);
     generator->height_noise_map_builder->Build();
+
+    generator->tree_noise_map_builder->SetDestSize(128, 128);
+    generator->tree_noise_map_builder->SetBounds(0.0, 128.0, 0.0, 128.0);
+    generator->tree_noise_map_builder->Build();
 }
 
 internal void generate_chunk(Chunk_Manager *manager, World_Generator *generator, Chunk *chunk) {
@@ -28,19 +47,42 @@ internal void generate_chunk(Chunk_Manager *manager, World_Generator *generator,
 
     for (int x = 0; x < CHUNK_SIZE; x++) {
         for (int z = 0; z < CHUNK_SIZE; z++) {
-            float height_map_value = *generator->height_noise_map->GetSlabPtr(x, z);
+            f32 height_map_value = *generator->height_noise_map->GetSlabPtr(x, z);
             int height = (int)floorf(CHUNK_SIZE * Abs(height_map_value));
-            height = Clamp(height, 10, 45);
+            height = Clamp(height, 12, 20);
 
-            for (int y = 0; y < height; y++) {
-                Block_ID block_type = BLOCK_STONE;
-                if (y == height - 1) {
-                    block_type = BLOCK_GRASS;
-                } else if (y >= 0.7f * height) {
-                    block_type = BLOCK_DIRT;
+            if (height < 8) {
+                for (int y = 0; y < height; y++) {
+                    Block_ID *block = BLOCK_AT(chunk, x, y, z);
+                    *block = BLOCK_WATER;
                 }
-                Block_ID *block = BLOCK_AT(chunk, x, y, z);
-                *block = block_type;
+            } else {
+                for (int y = 0; y < height; y++) {
+                    Block_ID block_type = BLOCK_STONE;
+                    if (y == height - 1) {
+                        block_type = BLOCK_GRASS;
+                    } else if (y >= 0.7f * height) {
+                        block_type = BLOCK_DIRT;
+                    }
+                    Block_ID *block = BLOCK_AT(chunk, x, y, z);
+                    *block = block_type;
+                }
+
+                f32 tree_value = *generator->tree_noise_map->GetSlabPtr(x, z);
+                bool generate_tree = false;
+                if (tree_value > 0.7f && tree_value < 0.8f) {
+                    generate_tree = true;
+                }
+
+                if (generate_tree) {
+                    int max_tree_y = height + 4;
+                    for (int y = height; y < max_tree_y; y++) {
+                        Block_ID *block = BLOCK_AT(chunk, x, y, z);
+                        *block = BLOCK_LOG;
+                    }
+                    Block_ID *block = BLOCK_AT(chunk, x, max_tree_y, z);
+                    *block = BLOCK_LEAVES;
+                }
             }
         }
     }
