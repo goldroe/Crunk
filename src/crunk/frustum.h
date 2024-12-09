3222dc337a262939a903a069c32be5ceb068a197
Carlos Rivera <cr4185320@gmail.com>
2024-12-08 20:27:37 -0600

    Update frustum.h

--- src/crunk/frustum.h
+++ src/crunk/frustum.h
@@ -5,4 +5,8 @@ struct Frustum {
     v4 planes[6];
 };
 
+inline internal bool point_behind_plane(v3 p, v4 plane);
+internal Frustum make_frustum(Camera camera, f32 near_z, f32 far_z);
+internal bool aabb_in_frustum(Frustum frustum, AABB box);
+
 #endif // FRUSTUM_H
