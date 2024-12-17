12c33b81d9da75387395d73db88820e95aed0ddb
Carlos Rivera <cr4185320@gmail.com>
2024-12-17 17:06:28 -0600

    Improve world generation, islands

--- src/crunk/world_gen.cpp
+++ src/crunk/world_gen.cpp
@@ -1,89 +1,107 @@
 
 internal void init_world_generator(World_Generator *generator, int seed) {
-    //@Note Height Map
-    generator->height_noise = new noise::module::Perlin();
-    generator->height_noise->SetSeed(seed);
-    generator->height_noise_map = new noise::utils::NoiseMap();
+    generator->height_noise = FastNoiseLite(seed);
+    generator->height_noise.SetFractalLacunarity(1.75f);
+    generator->height_noise.SetFractalOctaves(5);
+    generator->height_noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2); 
+    generator->height_noise.SetFractalType(FastNoiseLite::FractalType_FBm);
 
-    generator->height_noise_map_builder = new noise::utils::NoiseMapBuilderPlane();
-    generator->height_noise_map_builder->SetSourceModule(*generator->height_noise);
-    generator->height_noise_map_builder->SetDestNoiseMap(*generator->height_noise_map);
+    // //@Note Tree Map
+    // generator->tree_noise = new noise::module::Perlin();
+    // generator->tree_noise->SetSeed(seed);
+    // generator->tree_noise->SetOctaveCount(20);
+    // generator->tree_noise->SetLacunarity(2.0);
+    // generator->tree_noise->SetPersistence(1.0);
+    // generator->tree_noise->SetSeed(seed);
+    // generator->tree_noise->SetFrequency(0.5);
+    // generator->tree_noise_map = new noise::utils::NoiseMap();
 
-    //@Note Tree Map
-    generator->tree_noise = new noise::module::Perlin();
-    generator->tree_noise->SetSeed(seed);
-    generator->tree_noise->SetOctaveCount(20);
-    generator->tree_noise->SetLacunarity(2.0);
-    generator->tree_noise->SetPersistence(1.0);
-    generator->tree_noise->SetSeed(seed);
-    generator->tree_noise->SetFrequency(0.5);
-    generator->tree_noise_map = new noise::utils::NoiseMap();
-
-    generator->tree_noise_map_builder = new noise::utils::NoiseMapBuilderPlane();
-    generator->tree_noise_map_builder->SetSourceModule(*generator->tree_noise);
-    generator->tree_noise_map_builder->SetDestNoiseMap(*generator->tree_noise_map);
+    // generator->tree_noise_map_builder = new noise::utils::NoiseMapBuilderPlane();
+    // generator->tree_noise_map_builder->SetSourceModule(*generator->tree_noise);
+    // generator->tree_noise_map_builder->SetDestNoiseMap(*generator->tree_noise_map);
 
 }
 
-internal void update_world_generator(World_Generator *generator, s32 region_x, s32 region_z) {
-    generator->region_x = region_x;
-    generator->region_z = region_z;
+// internal void update_world_generator(World_Generator *generator, s32 region_x, s32 region_z) {
+//     generator->region_x = region_x;
+//     generator->region_z = region_z;
 
-    generator->height_noise_map_builder->SetDestSize(256, 256);
-    generator->height_noise_map_builder->SetBounds(1.0, 8.0, 2.0, 10.0);
-    generator->height_noise_map_builder->Build();
+//     generator->height_noise_map_builder->SetDestSize(256, 256);
+//     generator->height_noise_map_builder->SetBounds(1.0, 8.0, 2.0, 10.0);
+//     generator->height_noise_map_builder->Build();
 
-    generator->tree_noise_map_builder->SetDestSize(128, 128);
-    generator->tree_noise_map_builder->SetBounds(0.0, 128.0, 0.0, 128.0);
-    generator->tree_noise_map_builder->Build();
-}
+//     generator->tree_noise_map_builder->SetDestSize(128, 128);
+//     generator->tree_noise_map_builder->SetBounds(0.0, 128.0, 0.0, 128.0);
+//     generator->tree_noise_map_builder->Build();
+// }
 
 internal void generate_chunk(Chunk_Manager *manager, World_Generator *generator, Chunk *chunk) {
-    s32 region_x = chunk->position.x >> 5;
-    s32 region_z = chunk->position.z >> 5;
-    if (region_x != generator->region_x || region_z != generator->region_z) {
-        update_world_generator(generator, region_x, region_z);
-    }
+    V3_S32 position = chunk->position;
+    position.x *= CHUNK_SIZE;
+    position.z *= CHUNK_SIZE;
 
     for (int x = 0; x < CHUNK_SIZE; x++) {
         for (int z = 0; z < CHUNK_SIZE; z++) {
-            f32 height_map_value = *generator->height_noise_map->GetSlabPtr(x, z);
-            int height = (int)floorf(CHUNK_SIZE * Abs(height_map_value));
-            height = Clamp(height, 4, 20);
+            f32 noise_x = (position.x + (f32)x);
+            f32 noise_z = (position.z + (f32)z);
+            f32 noise_value = generator->height_noise.GetNoise(noise_x, noise_z) / 2.0f + 0.5f;
+            s32 height = 45 + (s32)(noise_value * 45);
 
-            if (height < 8) {
-            //     for (int y = 0; y < height; y++) {
-            //         Block_ID *block = block_at(chunk, x, y, z);
-            //         *block = BLOCK_WATER;
-            //     }
-            } else {
-                for (int y = 0; y < height; y++) {
-                    Block_ID block_type = BLOCK_STONE;
-                    if (y == height - 1) {
-                        block_type = BLOCK_GRASS;
-                    } else if (y >= 0.7f * height) {
-                        block_type = BLOCK_DIRT;
+            for (int y = 0; y < height; y++) {
+                Block_ID block_id = BLOCK_STONE;
+                int dy = height - y;
+
+                if (dy == 1) {
+                    if (y <= 64 && y >= 63) {
+                        block_id = BLOCK_SAND;
+                    } else if (y < 63) {
+                        block_id = BLOCK_STONE;
+                    } else {
+                        block_id = BLOCK_GRASS;
                     }
-                    Block_ID *block = block_at(chunk, x, y, z);
-                    *block = block_type;
-                }
-
-                f32 tree_value = *generator->tree_noise_map->GetSlabPtr(x, z);
-                bool generate_tree = false;
-                if (tree_value > 0.7f && tree_value < 0.8f) {
-                    generate_tree = true;
-                }
-
-                if (generate_tree) {
-                    int max_tree_y = height + 4;
-                    for (int y = height; y < max_tree_y; y++) {
-                        Block_ID *block = block_at(chunk, x, y, z);
-                        *block = BLOCK_LOG;
+                } else if (dy < 5) {
+                    if (dy < 64) {
+                        block_id = BLOCK_STONE;
+                    } else {
+                        block_id = BLOCK_DIRT; 
                     }
-                    Block_ID *block = block_at(chunk, x, max_tree_y, z);
-                    *block = BLOCK_LEAVES;
                 }
+                *block_at(chunk, x, y, z) = block_id;
             }
+
+            for (int y = 64; y >= height; y--) {
+                *block_at(chunk, x, y, z) = BLOCK_WATER;
+            }
+
+            *block_at(chunk, x, 0, z) = BLOCK_BEDROCK;
         }
     }
+
+    //     for (int y = 0; y < height; y++) {
+    //         Block_ID block_type = BLOCK_STONE;
+    //         if (y == height - 1) {
+    //             block_type = BLOCK_GRASS;
+    //         } else if (y >= 0.7f * height) {
+    //             block_type = BLOCK_DIRT;
+    //         }
+    //         Block_ID *block = block_at(chunk, x, y, z);
+    //         *block = block_type;
+    //     }
+
+    //     f32 tree_value = *generator->tree_noise_map->GetSlabPtr(x, z);
+    //     bool generate_tree = false;
+    //     if (tree_value > 0.7f && tree_value < 0.8f) {
+    //         generate_tree = true;
+    //     }
+
+    //     if (generate_tree) {
+    //         int max_tree_y = height + 4;
+    //         for (int y = height; y < max_tree_y; y++) {
+    //             Block_ID *block = block_at(chunk, x, y, z);
+    //             *block = BLOCK_LOG;
+    //         }
+    //         Block_ID *block = block_at(chunk, x, max_tree_y, z);
+    //         *block = BLOCK_LEAVES;
+    //     }
+    // }
 }
--- src/crunk/world_gen.h
+++ src/crunk/world_gen.h
@@ -1,29 +1,21 @@
 #ifndef WORLD_GEN_H
 #define WORLD_GEN_H
 
-struct Perlin_Map {
-    noise::module::Perlin perlin;
-    noise::utils::NoiseMap *map;
-    noise::utils::NoiseMapBuilderPlane *map_builder;
-};
-
 struct World_Generator {
     Arena *arena;
 
     s32 region_x;
     s32 region_z;
 
-    // FastNoiseLite perlin_noise;
-    // perlin_noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
-    // f32 *perlin_noise_map;
+    FastNoiseLite height_noise;
 
-    noise::module::Perlin *height_noise;
-    noise::utils::NoiseMap *height_noise_map;
-    noise::utils::NoiseMapBuilderPlane *height_noise_map_builder;
+    // noise::module::Perlin *height_noise;
+    // noise::utils::NoiseMap *height_noise_map;
+    // noise::utils::NoiseMapBuilderPlane *height_noise_map_builder;
 
-    noise::module::Perlin *tree_noise;
-    noise::utils::NoiseMap *tree_noise_map;
-    noise::utils::NoiseMapBuilderPlane *tree_noise_map_builder;
+    // noise::module::Perlin *tree_noise;
+    // noise::utils::NoiseMap *tree_noise_map;
+    // noise::utils::NoiseMapBuilderPlane *tree_noise_map_builder;
 };
 
 #endif // WORLD_GEN_H
