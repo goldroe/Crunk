cf13500e1fbe0bdf0534ae0727e744587a5be69b
Carlos Rivera <cr4185320@gmail.com>
2024-12-19 16:43:35 -0600

    dumb ambient occlusion "shading"

--- data/shaders/block.hlsl
+++ data/shaders/block.hlsl
@@ -33,10 +33,11 @@ struct Vertex_Output {
     float4 pos_w : SV_POSITION;
     float4 color : COL;
     float2 src : TEXCOORD;
+    float shading : FACE_SHADING;
 };
 
 uint get_face_orientation(uint data) {
-    return (data >> 24) & 0xFF;
+    return data & 0xFF;
 }
 
 float3 get_vertex_position(uint data) {
@@ -71,6 +72,12 @@ float4 get_color(uint col_id) {
 }
 
 Vertex_Output vs_main(Vertex_Input input) {
+    float face_shading[6] = {
+        1.0, 0.5, // TB
+        0.5, 0.8, // NS
+        0.5, 0.8 // EW 
+    };
+
     Vertex_Output output;
 
     float3 position = get_vertex_position(input.data.x);
@@ -78,10 +85,9 @@ Vertex_Output vs_main(Vertex_Input input) {
     uint face = get_face_orientation(input.data.x);
     uint uv_index = input.data.y & 0xFF;
 
-    float4 ambience = float4(0.1f, 0.1f, 0.1f, 0.75f);
+    // float4 ambience = float4(0.1f, 0.1f, 0.1f, 0.75f);
     uint light_level = get_light_level(input.data.y);
     float light = (light_level + 1) / 16.0f;
-
     float4 light_color = float4(light, light, light, 1.0);
     float4 base_color = get_color(get_color_id(input.data.y));
 
@@ -96,12 +102,14 @@ Vertex_Output vs_main(Vertex_Input input) {
 
     output.pos_w = mul(mvp, float4(position, 1));
     output.src = uv_buffer[uv_index];
-    output.color = light_color * base_color;
+    output.color = base_color;
+    output.shading = face_shading[face];
     return output;
 }
 
 float4 ps_main(Vertex_Output input) : SV_TARGET {
     float4 diffuse_color = tex.Sample(tex_sampler, input.src);
-    float4 final_color = diffuse_color * input.color;
+    float4 shade_color = float4(input.shading, input.shading, input.shading, 1.0);
+    float4 final_color = shade_color * diffuse_color * input.color;
     return final_color;
 }
